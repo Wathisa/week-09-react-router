30a323f0128bf8f903446ff830cad0061242b8f4
Wathisa <wathisa@gmail.com>
2026-04-27 14:37:45 +0700

    dynamic routes works

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:productId",
+        element: <ProductDetail />,
+      },
+    ],
   },
 ]);
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 export default function Layout() {
-  return <div>Navbar</div>;
+  return (
+    <div className="=min-h-screen bg-gray-100 flex flex-col">
+      <div className="bg-amber-500"> Navbar</div>
+
+      <div className="bg-cyan-800">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
